65b50425156ab279b9039f6deb5e65ab92f6370b
PasqualeIsabelli <pasquale0395@gmail.com>
2023-07-05 22:11:08 +0200

    add comments

--- js/main.js
+++ js/main.js
@@ -1,11 +1,17 @@
+// Variabili
 const squareContainer = document.querySelector(".square-container");
 const totalSquare = 10 * 10;
 
+// Ciclo
 for (let i = 1; i <= totalSquare; i++) {
+  // Creo elemento div
   let singleSquare = document.createElement("div");
+  // Aggiungo le classi che servono
   singleSquare.classList.add("fizz-square", "bg-primary", "fs-5", "fw-bold", "text-center", "lh-lg");
+  // Applico ad ogni elemento il numero preso da i
   singleSquare.innerHTML = i.toString();
-
+  
+  // Condizioni
   if (i % 3 === 0 && i % 5 === 0){
     singleSquare.classList.add("bg-danger");
     singleSquare.innerHTML = ("FizzBuzz");
